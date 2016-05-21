861d51d6dd0af033f0bfe7c481c7d33367fa99d4
Simon Goller <neosam+github@posteo.de>
2016-05-21 15:35:24 +0200

    Fixed hash generation on insert.

--- src/log.rs
+++ src/log.rs
@@ -46,9 +46,22 @@
 //! let first_hash: Hash = log.push(MyStruct{x: 42});
 //! let second_hash: Hash = log.push(MyStruct{x: 23});
 //!
+//! // The push method returns the hash value which can be used as key.
+//! assert_eq!("377194384a7432ebd8d8e0f19a1bcc17f115a220d48e280f8d75b6a5b43c3e1d",
+//!                &first_hash.as_string());
+//! assert_eq!("5894a38091d60a64cb6396edc2662c6460c3685b78b4381051dbc15ff30c5bcc",
+//!                &second_hash.as_string());
+//!
+//! // Inserting the same value again gives a completely different hash because
+//! // the hash also contains the privious entry.
+//! let third_hash: Hash = log.push(MyStruct{x: 23});
+//! assert_eq!("f87fa51292d72bb55a842b3f46c83adf71720a89abc3c7d89494d84458b57861",
+//!                &third_hash.as_string());
+//!
 //! // Verify entries
 //! assert_eq!(42, log.get(first_hash).unwrap().x);
 //! assert_eq!(23, log.get(second_hash).unwrap().x);
+//! assert_eq!(23, log.get(third_hash).unwrap().x);
 //!
 //! // Iterate over the entries
 //! // This log operates like a stack and will return the last (latest)
@@ -59,7 +72,8 @@
 //! }
 //!
 //! assert_eq!(23, res[0]);
-//! assert_eq!(42, res[1]);
+//! assert_eq!(23, res[1]);
+//! assert_eq!(42, res[2]);
 //! ```
 
 
@@ -130,6 +144,36 @@ impl<'a, L: Log<Item=T>, T: Hashable + 'a> Iterator for LogIteratorRef<'a, L, T>
 }
 
 
+pub struct LogIteratorHash<'a, L: Log<Item=T> + 'a, T: Hashable> {
+    log: &'a L,
+    hash: Option<Hash>
+}
+
+impl<'a, L: Log<Item=T>, T: Hashable + 'a> LogIteratorHash<'a, L, T> {
+    pub fn from_log(log: &'a L) -> LogIteratorHash<'a, L, T> {
+        LogIteratorHash {
+            log: log,
+            hash: log.head_hash()
+        }
+    }
+}
+
+impl<'a, L: Log<Item=T>, T: Hashable + 'a> Iterator for LogIteratorHash<'a, L, T> {
+    type Item = Hash;
+
+    fn next(&mut self) -> Option<Hash> {
+        match self.hash {
+            None => None,
+            Some(hash) => {
+                let value = self.log.get(hash);
+                self.hash = self.log.parent_hash(hash);
+                self.hash
+            }
+        }
+    } 
+}
+
+
 // ---- Defining HashLog types
 
 /// Stores one of the supported hash values.
@@ -137,6 +181,43 @@ impl<'a, L: Log<Item=T>, T: Hashable + 'a> Iterator for LogIteratorRef<'a, L, T>
 pub enum Hash {
     None,
     Sha3([u8; 32])
+}
+
+fn half_byte_to_string(byte: u8) -> String {
+    match byte {
+        0 => "0",
+        1 => "1",
+        2 => "2",
+        3 => "3",
+        4 => "4",
+        5 => "5",
+        6 => "6",
+        7 => "7",
+        8 => "8",
+        9 => "9",
+        10 => "a",
+        11 => "b",
+        12 => "c",
+        13 => "d",
+        14 => "e",
+        15 => "f",
+        _ => "?"
+    }.to_string()
+}
+
+fn byte_to_string(byte: u8) -> String {
+    let mut res = String::new();
+    res.push_str(&half_byte_to_string(byte / 16));
+    res.push_str(&half_byte_to_string(byte % 16));
+    res
+}
+
+fn bytes_to_string(bytes: &[u8]) -> String {
+    let mut res = String::new();
+    for byte in bytes {
+        res.push_str(&byte_to_string(*byte)); 
+    }
+    res
 }
 
 impl Hash {
@@ -147,11 +228,36 @@ impl Hash {
             &Hash::Sha3(x) => Box::new(x)
         }
     }
+
+    pub fn as_string(&self) -> String {
+        bytes_to_string(&*self.get_bytes())
+    }
+
+    pub fn hash_bytes(bytes: &[u8]) -> Hash {
+        let mut sha3 = Sha3::sha3_256();
+        sha3.input(bytes);
+        let mut res = [0u8; 32];
+        sha3.result(&mut res);
+        Hash::Sha3(res)
+    }
+
+    pub fn hash_with(&self, o: Hash) -> Hash {
+        let mut vec: Vec<u8> = Vec::new();
+        vec.extend_from_slice(&*self.get_bytes());
+        vec.extend_from_slice(&*o.get_bytes());
+        Hash::hash_bytes(vec.as_slice())
+    }
 }
 
 /// Can generate a hash type which represents the current type.
 pub trait Hashable {
     fn as_hash(&self) -> Hash;
+}
+
+impl Hashable for Hash {
+    fn as_hash(&self) -> Hash {
+        Hash::hash_bytes(&*self.get_bytes())
+    }
 }
 
 pub struct DefaultLogEntry<T: Hashable> {
@@ -170,6 +276,10 @@ pub struct DefaultLog<T: Hashable> {
 impl<T: Hashable> DefaultLog<T> {
     pub fn iter(&self) -> LogIteratorRef<DefaultLog<T>, T> {
         LogIteratorRef::from_log(self)
+    }
+
+    pub fn hash_iter(&self) -> LogIteratorHash<DefaultLog<T>, T> {
+        LogIteratorHash::from_log(self)
     }
 
     pub fn with_load_fn(mut self, load_fn: Box<Fn(Hash) -> Option<DefaultLogEntry<T>>>) -> DefaultLog<T> {
@@ -196,7 +306,11 @@ impl<T: Hashable> Log for DefaultLog<T> {
     }
 
     fn push(&mut self, t: T) -> Hash {
-        let hash = t.as_hash();
+        let entry_hash = t.as_hash();
+        let hash = match self.head {
+            None => entry_hash.as_hash(),
+            Some(head_hash) => entry_hash.hash_with(head_hash)
+        };
         let log_entry = DefaultLogEntry {
             entry: t,
             parent_hash: self.head
